bdc32c3cfbfb0d7b027e8b0ac137bc9685c8aaa9
Sam <165819040+smmartees@users.noreply.github.com>
2024-10-24 16:09:23 -0400

    Add project files.

---
+++ WhoWon/main.c
@@ -0,0 +1,9 @@
+// samp - CSCN71020 - fall24 - section 1
+
+// This assignment is to build a function to determine the outcome of 
+// a game of Rock, Paper, Scissors.
+
+int main(void) {
+
+	return 0;
+}
